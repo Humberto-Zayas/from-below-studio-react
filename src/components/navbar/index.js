7d9e2a8d47f68befbdaa6004040feca985206f52
Humberto Zayas <soundsinhd@gmail.com>
2023-01-17 22:22:27 -0500

    add icons and default navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -5,6 +5,15 @@ import React from "react";
 
 // import auth from "../../utils/auth";
 
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
 import logo from '../../images/fbs-logo-demo.png'
 console.log(logo);
 
@@ -12,9 +21,25 @@ console.log(logo);
 function Nav() {
 
   return (
-    <>
-      
-    </>
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
