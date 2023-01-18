e96918b5f74d9ace445a48f1982618e108154a05
Humberto Zayas <soundsinhd@gmail.com>
2023-01-17 22:15:39 -0500

    Clearing css and nav

---
+++ src/components/navbar/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+// import '../../css/nav.css';
+
+
+// import auth from "../../utils/auth";
+
+import logo from '../../images/fbs-logo-demo.png'
+console.log(logo);
+
+// Nav Function Start ///
+function Nav() {
+
+  return (
+    <>
+      
+    </>
+  );
+}
+
+
+
+export default Nav;
+
+
+
