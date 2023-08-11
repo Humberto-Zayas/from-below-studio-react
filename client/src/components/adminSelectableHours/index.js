04a7aba6f97a2c03576cd38ad70bcf8a69a81ce8
Humberto Zayas <soundsinhd@gmail.com>
2023-08-10 23:27:08 -0400

    remove graphql from admin hours

--- client/src/components/adminSelectableHours/index.js
+++ client/src/components/adminSelectableHours/index.js
@@ -1,75 +1,78 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import Switch from '@mui/material/Switch';
 import Grid from '@mui/material/Unstable_Grid2';
-import { useQuery } from '@apollo/client';
-import { QUERY_DAY } from '../../utils/queries';
-const label = { inputProps: { 'aria-label': 'Switch demo' } } // works
-const possibleHours = ['02 Hours/$70', '04 Hours/$130', '08 Hours/$270', '10 Hours/$340', 'Full Day 14+ Hours/$550'] // works
+
+const label = { inputProps: { 'aria-label': 'Switch demo' } };
+const possibleHours = ['02 Hours/$70', '04 Hours/$130', '08 Hours/$270', '10 Hours/$340', 'Full Day 14+ Hours/$550'];
 
 export default function AdminSelectableHours(props) {
-  const [checked, setChecked] = React.useState(possibleHours); // works
-  const [checkedPulled, setCheckedPulled] = React.useState(['']); // eh
-  const [available, setAvailable] = React.useState(true); //works ? need to test unavailable days...
-  const [selectedIndex, setSelectedIndex] = React.useState(0); // idk didnt make it
-  const { loading, data } = useQuery(QUERY_DAY, { // works ? need to test unavailable
-    variables: { date: props.recordingDate.toISOString().split('T')[0] }
-  });
+  const [checked, setChecked] = useState(possibleHours);
+  const [checkedPulled, setCheckedPulled] = useState(['']);
+  const [available, setAvailable] = useState(true);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [data, setData] = useState(null);
 
-  const hourOptionCheck = () => { // works locally but need to pass data up...
+  useEffect(() => {
+    // Fetch day data using the Fetch API
+    fetch(`/api/day?date=${props.recordingDate.toISOString().split('T')[0]}`)
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+      })
+      .catch(error => {
+        console.error('Error fetching day data:', error);
+      });
+  }, [props.recordingDate]);
+
+  const hourOptionCheck = () => {
     if (data && data.day !== null) {
-      const enabledObjects = data?.day?.hours.filter(obj => obj.enabled === true); // fine
-      const enabledNames = enabledObjects.map(obj => obj.hour); // fine 
-      console.log(enabledNames); // ["John", "Mark", "Sarah"] // fine local
-      setCheckedPulled([...enabledNames]) // fine local
-      callSetCheckedPulledHours(); // not working as should to parent
+      const enabledObjects = data?.day?.hours.filter(obj => obj.enabled === true);
+      const enabledNames = enabledObjects.map(obj => obj.hour);
+      setCheckedPulled([...enabledNames]);
+      callSetCheckedPulledHours();
     } else {
-      props.handleSetCheckedHours(checked) // works one time but not consistent
+      props.handleSetCheckedHours(checked);
     }
   }
 
-  const callSetCheckedPulledHours = () => { // eh no, works late
-    props.handleSetCheckedHours(checkedPulled)
+  const callSetCheckedPulledHours = () => {
+    props.handleSetCheckedHours(checkedPulled);
   }
 
-  const availabilityCheck = () => { // works ? need to test
-    if (!loading && data && data.day) {
-      console.log('availablilityCheck ran: ', data.day.disabled)
-      setAvailable(!data.day.disabled)
-      props.updateAvailability(available)
+  const availabilityCheck = () => {
+    if (data && data.day) {
+      setAvailable(!data.day.disabled);
+      props.updateAvailability(available);
     }
   }
 
-  // console.log('checkPulled state: ', checkedPulled)
-
-  const handleListItemClick = (event, index) => { // didnt make 
+  const handleListItemClick = (event, index) => {
     setSelectedIndex(index);
-    props.selectHours(event.target.innerText)
-    props.handleSetCheckedHours(checked)
+    props.selectHours(event.target.innerText);
+    props.handleSetCheckedHours(checked);
   };
 
-  const handleToggle = (value) => () => { // didnt make
+  const handleToggle = (value) => () => {
     const currentIndex = checked.indexOf(value);
     const newChecked = [...checked];
 
     if (currentIndex === -1) {
       newChecked.push(value);
       newChecked.sort();
-      props.handleSetCheckedHours(newChecked)
-
+      props.handleSetCheckedHours(newChecked);
     } else {
       newChecked.splice(currentIndex, 1);
       newChecked.sort();
-      props.handleSetCheckedHours(newChecked)
-
+      props.handleSetCheckedHours(newChecked);
     }
     setChecked(newChecked);
   };
 
-  const handleTogglePulled = (value) => () => { //copied but didnt make
+  const handleTogglePulled = (value) => () => {
     const currentIndex = checkedPulled.indexOf(value);
     const newChecked = [...checkedPulled];
 
@@ -78,29 +81,27 @@ export default function AdminSelectableHours(props) {
     } else {
       newChecked.splice(currentIndex, 1);
     }
-    // send setCheckedPull back
     setCheckedPulled(newChecked);
   };
 
-  React.useEffect(() => { // works i guess
-    hourOptionCheck()
+  useEffect(() => {
+    hourOptionCheck();
     availabilityCheck();
-  }, [data])
+  }, [data]);
 
   return (
     <Box sx={{ width: '100%', maxWidth: 900, bgcolor: 'transparent' }}>
-      <Switch defaultChecked={available} onChange={() => { setAvailable(!available)}} {...label} />
+      <Switch defaultChecked={available} onChange={() => { setAvailable(!available) }} {...label} />
       {available ? <span style={{ color: '#e6e6e6', fontSize: '1.5em' }}>Available</span> : <span style={{ color: '#e6e6e6', fontSize: '1.5em' }}>Unavailable</span>}
       <Grid container spacing={2}>
         <Grid xs={6}>
-          {data?.day !== null ?
+          
             <List className='recording-pricing-2-desktop' component="nav" aria-label="Recording Package Selection">
               {data?.day.hours.map((item, index) => (
                 <ListItemButton
                   disabled={!available}
                   key={item.hour}
                   selected={selectedIndex === index}
-                // onClick={(event) => handleListItemClick(event, index)}
                 >
                   <ListItemText style={{ color: '#bcbcbc' }} primary={item.hour} />
                   <Switch
@@ -116,94 +117,30 @@ export default function AdminSelectableHours(props) {
             </List>
             :
             <List className='recording-pricing-2-desktop' component="nav" aria-label="Recording Package Selection">
-              <ListItemButton
-                disabled={!available}
-                // selected={selectedIndex === 0}
-                // onClick={(event) => handleListItemClick(event, 0)}
-              >
-                <ListItemText style={{ color: '#bcbcbc' }} primary="2 Hours/$70" />
-                <Switch
-                  edge="end"
-
-                  onChange={handleToggle('02 Hours/$70')}
-                  checked={checked.indexOf('02 Hours/$70') !== -1}
-                  inputProps={{
-                    'aria-labelledby': '02 Hours/$70',
-                  }}
-                />
-              </ListItemButton>
-              <ListItemButton
-                disabled={!available}
-                // selected={selectedIndex === 1}
-                // onClick={(event) => handleListItemClick(event, 1)}
-              >
-                <ListItemText style={{ color: '#bcbcbc' }} primary="4 Hours/$130" />
-                <Switch
-                  edge="end"
-
-                  onChange={handleToggle('04 Hours/$130')}
-                  checked={checked.indexOf('04 Hours/$130') !== -1}
-                  inputProps={{
-                    'aria-labelledby': '04 Hours/$130',
-                  }}
-                />
-              </ListItemButton>
-              <ListItemButton
-                disabled={!available}
-                // selected={selectedIndex === 2}
-                // onClick={(event) => handleListItemClick(event, 2)}
-              >
-                <ListItemText style={{ color: '#bcbcbc' }} primary="8 Hours/$270" />
-                <Switch
-                  edge="end"
-
-                  onChange={handleToggle('08 Hours/$270')}
-                  checked={checked.indexOf('08 Hours/$270') !== -1}
-                  inputProps={{
-                    'aria-labelledby': '08 Hours/$270',
-                  }}
-                />
-              </ListItemButton>
-              <ListItemButton
-                disabled={!available}
-                // selected={selectedIndex === 3}
-                // onClick={(event) => handleListItemClick(event, 3)}
-              >
-                <ListItemText style={{ color: '#bcbcbc' }} primary="10 Hours/$340" />
-                <Switch
-                  edge="end"
-
-                  onChange={handleToggle('10 Hours/$340')}
-                  checked={checked.indexOf('10 Hours/$340') !== -1}
-                  inputProps={{
-                    'aria-labelledby': '10 Hours/$340',
-                  }}
-                />
-              </ListItemButton>
-              <ListItemButton
-                disabled={!available}
-                // selected={selectedIndex === 4}
-                // onClick={(event) => handleListItemClick(event, 4)}
-              >
-                <ListItemText style={{ color: '#bcbcbc' }} primary="Full Day 14+ Hours/$550" />
-                <Switch
-                  edge="end"
-
-                  onChange={handleToggle('Full Day 14+ Hours/$550')}
-                  checked={checked.indexOf('Full Day 14+ Hours/$550') !== -1}
-                  inputProps={{
-                    'aria-labelledby': 'Full Day 14+ Hours/$550',
-                  }}
-                />
-              </ListItemButton>
+              {possibleHours.map((item, index) => (
+                <ListItemButton
+                  disabled={!available}
+                  key={item}
+                >
+                  <ListItemText style={{ color: '#bcbcbc' }} primary={item} />
+                  <Switch
+                    edge="end"
+                    onChange={handleToggle(item)}
+                    checked={checked.indexOf(item) !== -1}
+                    inputProps={{
+                      'aria-labelledby': item,
+                    }}
+                  />
+                </ListItemButton>
+              ))}
             </List>
-          }
+
         </Grid>
         <Grid xs={6}>
-          {data?.day !== null ? 
-            <p style={{color: 'white', fontSize: '1.3em'}}>check pulled: <br></br>{checkedPulled}</p> 
+          {data?.day !== null ?
+            <p style={{ color: 'white', fontSize: '1.3em' }}>check pulled: <br></br>{checkedPulled}</p>
             :
-            <p style={{color: 'white', fontSize: '1.3em'}}>checked: <br></br>{checked}</p>
+            <p style={{ color: 'white', fontSize: '1.3em' }}>checked: <br></br>{checked}</p>
           }
         </Grid>
       </Grid>
