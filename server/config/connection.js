bbcb325a512e20c6c94df8146c758a6addecf16b
Humberto Zayas <soundsinhd@gmail.com>
2023-08-14 16:23:47 -0400

    update connection file for railway

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,28 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/from-below',
-  {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-  }
-);
+let connectionString;
 
-module.exports = mongoose.connection;
+if (process.env.NODE_ENV === 'production') {
+  const mongoUser = process.env.MONGOUSER;
+  const mongoPassword = process.env.MONGOPASSWORD;
+  const mongoHost = process.env.MONGOHOST;
+  const mongoPort = process.env.MONGOPORT;
+
+  connectionString = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}`;
+} else {
+  connectionString = 'mongodb://127.0.0.1:27017/from-below';
+}
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+module.exports = db;
