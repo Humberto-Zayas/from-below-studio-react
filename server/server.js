b3662a62089999dd7f87c1a5f0ed0a63abe61dff
Humberto Zayas <soundsinhd@gmail.com>
2023-08-21 22:15:54 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -235,7 +235,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get("/*", (req, res) => {
+// Add a route for /admin
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
