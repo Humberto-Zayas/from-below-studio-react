b7a73615e7c3c23474101981844bb2084f47cf62
Humberto Zayas <soundsinhd@gmail.com>
2023-08-22 13:51:29 -0400

    setup admin bookings component for admin

---
+++ client/src/components/AdminBookings/index.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import { Typography, Card, CardContent, CardHeader, Container, Grid } from '@mui/material';
+
+const AdminBookings = () => {
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    // Fetch bookings from the API
+    fetch('/api/bookings')
+      .then((response) => response.json())
+      .then((data) => {
+        setBookings(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching bookings:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>
+        Bookings
+      </Typography>
+      <Container maxWidth="md">
+        {bookings.length === 0 ? (
+          <Typography variant="body1">No bookings available.</Typography>
+        ) : (
+          <Grid container spacing={3}>
+            {bookings.map((booking) => (
+              <Grid item xs={12} md={6} key={booking._id}>
+                <Card>
+                  <CardHeader title={`Booking ID: ${booking._id}`} />
+                  <CardContent>
+                    <Typography variant="body1">Name: {booking.name}</Typography>
+                    <Typography variant="body1">Email: {booking.email}</Typography>
+                    <Typography variant="body1">Phone Number: {booking.phoneNumber}</Typography>
+                    <Typography variant="body1">Message: {booking.message}</Typography>
+                    <Typography variant="body1">How Did You Hear About Us: {booking.hearAboutUs}</Typography>
+                    <Typography variant="body1">Date: {booking.date}</Typography>
+                    <Typography variant="body1">Hours: {booking.hours.join(', ')}</Typography>
+                    <Typography variant="body1">Status: {booking.status}</Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        )}
+      </Container>
+    </div>
+  );
+};
+
+export default AdminBookings;
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AdminDateHours from '../components/AdminDateHours';
-import { Typography, Container } from '@mui/material';
+import AdminBookings from '../components/AdminBookings'; // Import the bookings component
+import { Typography, Container, Drawer, List, ListItem, ListItemIcon, ListItemText, Divider } from '@mui/material';
+import { CalendarToday, ListAlt } from '@mui/icons-material';
 
 const Admin = () => {
+  const [selectedComponent, setSelectedComponent] = useState('dateHours'); // Default component is dateHours
+  const toggleComponent = (component) => {
+    setSelectedComponent(component);
+  };
+
   return (
     <div style={{ minHeight: '100vh', height: 'auto', backgroundColor: '#f4f6f8' }} className='about'>
       <Container maxWidth="md" style={{ paddingTop: '1em' }}>
-        <Typography className='hero-h1 heading oswald' variant="h2" component="h1" style={{ marginBottom: '1em', color: 'white' }}>Admin <span className='text-span-2'>Panel</span></Typography>
-        <AdminDateHours />
+        <Typography className='hero-h1 heading oswald' variant="h2" component="h1" style={{ marginBottom: '1em', color: 'white' }}>
+          Admin <span className='text-span-2'>Panel</span>
+        </Typography>
+        
+        {/* Side drawer */}
+        <Drawer anchor="left" open={true} variant="permanent" sx={{ width: 200 }}>
+          <List>
+            <ListItem button onClick={() => toggleComponent('dateHours')}>
+              <ListItemIcon>
+                <CalendarToday />
+              </ListItemIcon>
+              <ListItemText primary="Calendar" />
+            </ListItem>
+            <ListItem button onClick={() => toggleComponent('bookings')}>
+              <ListItemIcon>
+                <ListAlt />
+              </ListItemIcon>
+              <ListItemText primary="Bookings" />
+            </ListItem>
+          </List>
+          <Divider />
+        </Drawer>
+
+        {/* Display selected component */}
+        {selectedComponent === 'dateHours' && <AdminDateHours />}
+        {selectedComponent === 'bookings' && <AdminBookings />}
       </Container>
     </div>
   );
